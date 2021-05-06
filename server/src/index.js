d20aee75e8433fce9402ee24eaccc2a241079eaa
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-05-06 20:25:49 +0200

    Updated index.js with mongoose + the logs router

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'process.env.CORS_ORIGIN',
+}));
 
 app.get('/', (req, res) => {
   res.json({
@@ -15,11 +27,13 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/logs', logs);
+
 app.use(middlewares.notFound);
 
 app.use(middlewares.errorHandler);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Listening at localhost:${port}`);
 });
