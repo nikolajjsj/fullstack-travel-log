f3ad79b0f331734a1f1cfff7ce1dcf0913099cb1
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-05-06 20:44:55 +0200

    Updated post method for logs route

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const LogEntry = require('../models/LogEntry');
+
 router.get('/', (req, res) => {
   res.json({
     message: 'logs',
   });
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  // post a log to the database
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
 });
 
 module.exports = router;
