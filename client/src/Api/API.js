0b0bb10ae984142a810a1a11c1006a52a89ba75b
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-05-13 16:57:34 +0200

    Added function to add a log entry

--- client/src/Api/API.js
+++ client/src/Api/API.js
@@ -8,3 +8,19 @@ export async function fetchLogEntries() {
     console.error(error);
   }
 }
+
+export async function addLogEntry(entry) {
+  try {
+    const response = await fetch(`${API_URL}/api/logs`, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(entry),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    throw(error);
+  }
+}
